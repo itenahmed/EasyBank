5d061e050c68643943f81d8d100945dcf2533c02
itenahmed <itenahmed00@yahoo.com>
2026-01-08 15:27:20 +0200

    Initial commit - course project

---
+++ section2/accounts/src/main/java/com/easybytes/accounts/Controller/AccountsController.java
@@ -0,0 +1,11 @@
+package com.easybytes.accounts.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class AccountsController {
+    @GetMapping("sayhello")
+    public String sayHello() {
+        return "hi World";
+    }
+
+}
